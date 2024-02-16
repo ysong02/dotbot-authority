b8bc5ab80bed700d1bd7f08f963a11a66e596249
Geovane Fedrecheski <geonnave@gmail.com>
2024-02-16 16:51:09 +0100

    feat(frontend): add timestamp and table

--- dotbot_manager/frontend/src/App.jsx
+++ dotbot_manager/frontend/src/App.jsx
@@ -1,36 +1,75 @@
 import { useState } from 'react'
+import moment from 'moment'
+
 import './App.css'
 
 let acl = [1, 2, 3]
-let joined_dotbots = {
-  1: {
-    timestamp: 1708095439,
+let joined_dotbots_log = [
+  {
+    id: 1,
+    timestamp: moment().unix(),
+    authorized: true,
+  },
+  {
+    id: 4,
+    timestamp: moment().unix() - 1234,
+    authorized: false,
   }
+]
+function JoinedDotbot({ id, timestamp, authorized }) {
+  timestamp = moment(timestamp * 1000).format('YYYY-MM-DD HH:mm:ss');
+  return (
+    <tr>
+      <td className="firstLogCell">{timestamp}</td>
+      <td className="logCell">{id}</td>
+      <td className="logCell">{authorized ? "✅ Authorized" : "❌ Unauthorized"}</td>
+    </tr>
+  );
+}
+
+function JoinedDotbots({ dotbots }) {
+  return (
+    <div>
+      <h2>Joined DotBots Log:</h2>
+      <div style={{ display: "inline-block", minWidth: "50%" }}>
+        <table style={{ borderCollapse: "collapse" }}>
+          <thead>
+            <tr>
+              <th>Timestamp</th>
+              <th>ID</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dotbots.map((dotbot) => (
+              <JoinedDotbot key={dotbot.id} id={dotbot.id} timestamp={dotbot.timestamp} authorized={dotbot.authorized} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+function DotbotACL({ acl }) {
+  return (
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <h2 style={{ marginRight: "10px" }}>Allowed DotBots:</h2>
+      <div>
+        {acl.map((id) => (
+          <span style={{ margin: 5, padding: 5, border: "1px solid white" }} key={id}>{id}</span>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 function Dashboard() {
   return (
     <div>
       <h1>Dotbot Manager</h1>
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <h2 style={{ marginRight: "10px" }}>Access Control List:</h2>
-        <div>
-          {acl.map((id) => (
-            <span style={{ margin: 5, padding: 5, border: "1px solid white" }} key={id}>{id}</span>
-          ))}
-        </div>
-      </div>
-      <div >
-        <h2 >Joined DotBots:</h2>
-        <div>
-          {/* {acl.map((id) => ( */}
-          {Object.entries(joined_dotbots).map((dotbot) => {
-            let id = dotbot[0]
-            let timestamp = new Date(dotbot[1].timestamp * 1000).toDateString()
-            return <span style={{ margin: 5, padding: 5, border: "1px solid white" }} key={id}>{timestamp}: {id}</span>
-          })}
-        </div>
-      </div>
+      <DotbotACL acl={acl} />
+      <JoinedDotbots dotbots={joined_dotbots_log} />
     </div>
   )
 }
